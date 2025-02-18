80fac718dff408aff91735d8b83e89bad3ba49c9
josepht273 <josephtadessem+labOne@gmail.com>
2025-02-18 05:25:37 -0800

    Refactor insertion sort implementation and remove redundant methods

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -2,55 +2,48 @@
 
 using namespace std;
 
-void insertionSortMethodOne(int arr[], int n){
+void insertionSort(int arr[], int n){
     int innerCount = 0;
     int count = 0;
-    for (size_t i = 0; i < 5; i++)
-    {
-        count += 1;
-        int temp = arr[i];
-        for (size_t j = i; j < 5; j++)
-        {
-            innerCount += 1;
-            if (temp > arr[j])
-            {
-                arr[i] = arr[j];
-                arr[j] = temp;
-                temp = arr[i];
-            }
-        }
-    }
-
-    cout << "Outer loop count: " << count << endl;
-    cout << "Inner loop count: " << innerCount << endl;
-}
-
-void insertionSortMethodTwo(int arr[], int n){
-    int innerCount = 0;
-    int count = 0;
-    
+    int j, key;
     for (size_t i = 1; i < 5; i++)
     {
         count += 1;
-        int key = arr[i]; // Assigns the value of the current index to key
-        int j = i - 1; // Assigns the value of the previous index to j
-        while (j >= 0 && arr[j] > key) // 
+        key = arr[i];
+        j = i - 1;
+        while (j >= 0 && key < arr[j])
         {
             innerCount += 1;
-            arr[j + 1] = arr[j]; // Assigns the value of the previous index to the current index
-            j = j - 1; // Decrements the value of j by 1
+            arr[j + 1] = arr[j];
+            j = j - 1;
         }
-        arr[j + 1] = key; // Assigns the value of key to the current index
+        arr[j + 1] = key;
     }
 
     cout << "Outer loop count: " << count << endl;
     cout << "Inner loop count: " << innerCount << endl;
 }
 
+
 int main(){
-    int numbers[5] = {2, 2, 2, 1, 3};
+    int numbers[5] = {1, 5, 2, 4, 3};
+    int j, key;
+
+
+    for (size_t i = 1; i < 5; i++)
+    {
+        key = numbers[i];
+        j = i - 1;
+        while (j >= 0 && key > numbers[j])
+        {
+            numbers[j + 1] = numbers[j];
+            j = j - 1;
+        }
+        numbers[j + 1] = key;
+    }
+
+    // insertionSort(numbers, 5);
     
-    insertionSortMethodTwo(numbers, 5);
 
     cout << "Sorted values\n";
     for (size_t i = 0; i < 5; i++)
