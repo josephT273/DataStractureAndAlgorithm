f1ae50af54babd7089afe63ee46c6039a5558d8e
josepht273 <josephtadessem+labone@gmail.com>
2025-03-04 15:52:54 +0300

    Insertion, selection and bubble sort

---
+++ sort.cpp
@@ -0,0 +1,66 @@
+#include<iostream>
+
+using namespace std;
+
+void selectionSort(int numbers[], int n){
+    for(size_t i = 0; i < n; i++){
+        int smallestIndex = i;
+        for (size_t j = i + 1; j < n; j++)
+        {
+            if (numbers[smallestIndex] > numbers[j])
+            {
+                smallestIndex = j;
+            }
+        }
+        swap(numbers[smallestIndex], numbers[i]);
+    }
+
+    cout << "\nSelection sort\n";
+    for(size_t i = 0; i < n; i++){
+        cout << numbers[i] << " ";
+    }
+}
+
+void insertionSort(int numbers[], int n){
+    for (int i = 1; i < n; i++) {
+        int temp = numbers[i];
+        int j = i - 1;
+        while (j >= 0 && numbers[j] > temp) {
+            numbers[j + 1] = numbers[j];
+            j--;
+        }
+        numbers[j + 1] = temp;
+    }
+
+    cout << "\nInsertion sort result:\n";
+    for (int i = 0; i < n; i++) {
+        cout << numbers[i] << " ";
+    }
+    cout << endl;
+}
+
+void bubbleSort(int numbers[], int n){
+    for(size_t i = 0; i < n; i++){
+        for (size_t j = 0; j < n - i - 1; j++)
+        {
+            if (numbers[j] > numbers[j + 1])
+            {
+                swap(numbers[j], numbers[j + 1]);
+            }
+        }
+        
+    }
+    cout << endl << "Bubble sort\n";
+    for(size_t i = 0; i < n; i++){
+        cout << numbers[i] << " ";
+    }
+}
+int main(){
+    int numbers[5] = {-2, 3, -1, 0, 5};
+
+    selectionSort(numbers, 5);
+    insertionSort(numbers, 5);
+    bubbleSort(numbers, 5);
+    
+    return 0;
+}
